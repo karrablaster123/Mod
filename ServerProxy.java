a920a5ad11989378f67730f3edafbc5037786b7f
karrablaster123 <uddhava2m@gmail.com>
2018-01-06 16:37:24 +0530

    Delete ServerProxy.java

--- ServerProxy.java
+++
@@ -1,8 +0,0 @@
-package km.proxy;
-
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.relauncher.Side;
-@EventBusSubscriber(Side.SERVER)
-public class ServerProxy extends CommonProxy {
-
-}
