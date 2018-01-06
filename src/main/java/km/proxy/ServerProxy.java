6678063ba56dc6183629b51771c187ad421dc223
karrablaster123 <uddhava2m@gmail.com>
2018-01-06 16:38:25 +0530

    Add files via upload

---
+++ src/main/java/km/proxy/ServerProxy.java
@@ -0,0 +1,8 @@
+package km.proxy;
+
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.relauncher.Side;
+@EventBusSubscriber(Side.SERVER)
+public class ServerProxy extends CommonProxy {
+
+}
